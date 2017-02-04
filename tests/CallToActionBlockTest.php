e6b270205a781e365a25064b94886bf9c5a2bde6
Jason Irish <jirish@dynamicagency.com>
2017-02-04 15:33:46 -0600

    ContentBlock - templates
    Linkable
    * fixed naming conflict `Link`, replaced with `BlockLink`
    * added `button` template, option to display as text or button in CMS
    
    Fixed related issues in `CallToAction` block
    
    fixes #22
    ping #4

---
+++ tests/CallToActionBlockTest.php
@@ -0,0 +1,20 @@
+<?php
+
+class CallToActionBlockTest extends SapphireTest
+{
+    /**
+     * @var string
+     */
+    protected static $fixture_file = 'dynamic-blocks/tests/Fixtures.yml';
+
+    /**
+     *
+     */
+    public function testGetCMSFields()
+    {
+        $object = $this->objFromFixture('CallToActionBlock', 'one');
+        $fields = $object->getCMSFields();
+        $this->assertInstanceOf('FieldList', $fields);
+        $this->assertNotNull($fields->dataFieldByName('BlockLinkID'));
+    }
+}
